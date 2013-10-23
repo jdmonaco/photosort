cd3a0fe5a6117ff465305f3d05cad3c83d038664
Joe Monaco <self@joemona.co>
2013-10-23 14:02:28 -0400

    Reorganized some of the code; added os.getmtime fallback (h/t @jamiepinkham); minor tweaks

--- organize-photos.py
+++ organize-photos.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
 """
-organize_photos.py - Organize an unstructured folder of photo files by date
+organize-photos.py - Organize an unstructured folder tree of photos by date
 
-Note: This is a minor rewrite of @cliss's extension [1,2] of Dr. Drang's 
-photo management scripts [3].
+Note: This is a minor rewrite of @cliss's extension [1,2] of @drdrang's 
+photo management scripts [3], and includes a tweak from @jamiepinkham [4,5].
 
 [1] https://gist.github.com/cliss/6854904
 [2] http://tumblr.caseyliss.com/day/2013/10/06
-[2] http://www.leancrew.com/all-this/2013/10/photo-management-via-the-finder/
+[3] http://www.leancrew.com/all-this/2013/10/photo-management-via-the-finder/
+[4] https://gist.github.com/jamiepinkham/6984369
+[5] https://twitter.com/drdrang/status/389952079763996672
 """
 
 import sys
@@ -16,31 +18,33 @@ import os, os.path
 import subprocess
 from datetime import datetime
 
-
-## Edit these home-relative paths to set input and output locations
+## Edit these paths (relative to home) to set input and output locations
 sourcePath = 'Desktop/Photos to Organize'
 destPath = 'Pictures/Photos'
 ## No more editing (unless you're fixing/improving the script)
 
-
-sourceDir = os.path.join(os.environ['HOME'], sourcePath)
-destDir = os.path.join(os.environ['HOME'], destPath)
-errorDir = os.path.join(destDir, 'Unsorted')
-
-print 'Moving from %s to %s.' % (sourceDir, destDir)
-
 def get_date_time_of_photo(f):
-    cDate = subprocess.check_output(['sips', '-g', 'creation', f])
-    cDate = cDate.split('\n')[1].lstrip().split(': ')[1]
-    return datetime.strptime(cDate, "%Y:%m:%d %H:%M:%S")
+    try:
+        cDate = subprocess.check_output(['sips', '-g', 'creation', f])
+        cDate = cDate.split('\n')[1].lstrip().split(': ')[1]
+        return datetime.strptime(cDate, "%Y:%m:%d %H:%M:%S")
+    except:
+        return datetime.fromtimestamp(os.path.getmtime(f))
 
 def get_source_photo_filenames(d):
     p = []
-    is_photo = lambda f: f[-4:].lower() == '.jpg' or f[-5:].lower() == '.jpeg'
-    for dirpath, dirnames, filenames in os.walk(sourceDir):
-        path = os.path.join(sourceDir, dirpath)
+    is_photo = lambda f: os.path.splitext(f)[1].lower() in ('.jpg', '.jpeg', '.jpe')
+    for dirpath, dirnames, filenames in os.walk(d):        
+        path = os.path.join(d, dirpath)
         p.extend(map(lambda f: os.path.join(path, f), filter(is_photo, filenames)))
     return p
+
+home = os.environ['HOME']
+sourceDir = os.path.join(home, sourcePath)
+destDir = os.path.join(home, destPath)
+errorDir = os.path.join(destDir, 'Unsorted')
+print 'Moving from %s to %s.' % (sourceDir, destDir)
+
 photos = get_source_photo_filenames(sourceDir)
 print 'Found %d photos to process.' % len(photos)
 
@@ -55,7 +59,7 @@ fmt = "%Y-%m-%d %H-%M-%S"
 problems = []
 
 # Open a log file to record copy operations and errors
-logfd = file(os.path.join(destDir, 'organize_photos.log'), 'w')
+logfd = file(os.path.join(destDir, 'organize-photos.log'), 'w')
 
 for original in photos:
     suffix = 'a'
@@ -88,9 +92,9 @@ for original in photos:
         print >>logfd, 'Copied: %s -> %s' % (original, duplicate)
         
     except Exception:
-        unsorted_photo = os.path.join(errorDir, os.path.split(original)[1])
+        unsorted_photo = os.path.join(errorDir, os.path.basename(original))
         subprocess.call(['cp', '-p', original, unsorted_photo])
-        problems.append(original[len(os.environ['HOME']):])
+        problems.append(original[len(home):])
         print >>logfd, 'Error: unable to copy %s' % original
         
     except:
@@ -100,7 +104,7 @@ if len(problems) > 0:
     print "\nProblem files:"
     print "\n\t".join(problems)
     print "These can be found in: %s" % errorDir
-else:
+elif len(photos):
     sys.stdout.write('\n')
     
 logfd.close()
